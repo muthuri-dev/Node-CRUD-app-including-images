557a21c813d839eb9adc771fa25264f488f6a3cf
kennedy muthuri <muthurikennedy082@gmail.com>
2022-06-17 16:29:34 +0300

    deleted some comments

--- models/model.js
+++ models/model.js
@@ -34,27 +34,3 @@ const dataSchema=new schema({
 
 const userData=mongoose.model('userData',dataSchema);
 module.exports=userData;
-/*
-const mongoose=require('mongoose');
-const schema=mongoose.Schema;
-
-//Creating collection and model
-const imageCrudSchema=new schema({
-    name:{
-        type:String,
-        required:true
-    },
-    image:{
-        type:String,
-        required:true
-    },
-    created:{
-        type:Date,
-        required:true,
-        default:Date.now
-    }
-});
-const images=mongoose.model('images',imageCrudSchema);
-
-//Exporting the model
-module.exports=images;*/
